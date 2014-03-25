24461552eafdf1629ce2effeef1e05919195e6cb
Aurelia Moser <auremoser@gmail.com>
2014-03-25 20:22:25 +0300

    added internaltionalized chart dupe, test, flip axes and language translations

---
+++ js/intl.js
@@ -0,0 +1,165 @@
+(function() {
+	'use strict'
+	/*
+	 * IMPORT + PARSE DATA
+	 */
+	var posts = [];
+	var trust = [];
+	var untrust = [];
+	
+	var csv = dsv(',');
+	// get data from csv, pass to parse function
+	$.ajax({
+		url: 'data/data-posts.csv',
+		success: parseData
+	});
+	
+	// split D/M/YYYY format, month is 0 indexed (whaaat?) -> correct
+	function parseData(str) {
+		var data = csv.parse(str);
+		data.forEach(function(d) {
+			var mdy = d.date.split('/');
+			var date = new Date(mdy[2], mdy[0] - 1, mdy[1]);
+			d.date = date.getTime()
+		})
+	
+		data.sort(function(a, b) { return a.date - b.date });
+	
+		data.forEach(function(d) {
+			posts.push([d.date, +d.posts]);
+			trust.push([d.date, +d.trust]);
+			untrust.push([d.date, +d.untrust]);
+		});
+		
+		renderChart(data);
+	};
+	/*
+	 * DRAW CHART
+	 */
+	 // experimenting with example of flipped chart for internationalization:
+	 // http://www.highcharts.com/docs/advanced-chart-features/internationalization
+	 // edit lang via setOptions config
+	Highcharts.setOptions({
+		lang: {
+			contextButtonTitle: 'الرسم البياني قائمة السياق',
+			decimalPoint: ',',
+			downloadJPEG: 'تحميل JPG',
+			downloadPDF: 'تحميل PDF',
+			downloadPNG: 'تحميل PNG',
+			downloadSVG: 'تحميل SVG',
+			months: ['يناير', 'فبراير', 'مارس', 'أبريل/إبريل', 'مايو', 'يونيو/يونية', 'يوليو/يولية', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'],
+			printChart: 'طباعة الرسم البياني',
+			resetZoom: 'إعادة تعيين التكبير',
+			resetZoomTitle: 'إعادة تعيين مستوى التكبير 1:1',
+			shortMonths: ['يناير', 'فبراير', 'مارس', 'أبريل/إبريل', 'مايو', 'يونيو/يونية', 'يوليو/يولية', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'],
+			thousandsSep: '.',
+			weekdays: ['يوم الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت', 'الأحد']
+		}
+	});
+
+	function renderChart(data) {
+		console.log(data);
+		$('#intl').highcharts('StockChart', {
+			colors: [
+				// colors taken from the palette on this site: http://www.electionguide.org/map/
+				'#E9322D', '#46A546', '#2C81BA' , '#EC7063', '#FBD8DB', '#666'
+			],
+			rangeSelector: {
+			// defaults to most recent time for filter (so, 3M=3months from last date)
+				enabled: true,
+				buttons: [{
+					type: 'month',
+					count: 1,
+					text: '1M'
+				}, {
+					type: 'month',
+					count: 3,
+					text: '3M'
+				}, {
+					type: 'year',
+					count: 1,
+					text: '1Y'
+				}, {
+					type: 'all',
+					text: 'All'
+				}],
+				buttonSpacing: 2,
+				buttonTheme: {
+					stroke: 2,
+					r: 5,
+					style: {
+						color: 'E9322D'
+					},
+					states: {
+						hover: {
+							fill: '#FBD8DB'
+						},
+						select: {
+							fill: '#E9322D',
+							style: {
+								color: 'white'
+							}
+						}
+					}
+				},
+				inputBoxBorderColor: '#EC7063'
+			},
+			credits: {
+				enabled: false
+			},
+			title: {
+				text: 'IFES تقرير تعقب',
+				style: {
+					'color': '#E9322D'
+				},
+				useHTML: Highcharts.hasBidiBug
+			},
+			xAxis: {
+				type: 'datetime',
+				reversed: true
+			},
+			yAxis: {
+				title: {
+					align: 'middle',
+					text: 'عدد من تقارير',
+					style: {
+						'color': '#EC7063'
+					},
+					useHTML: Highcharts.hasBidiBug
+				},
+				opposite: true
+			},
+			legend: {
+				useHTML: Highcharts.hasBidiBug
+			},
+			navigator: {
+				enabled: true
+				// series: {
+				// 	color: '#FBD8DB'
+				// }
+			},
+			scrollbar: {
+				enabled: true
+				// barBackgroundColor: '#EC7063',
+				// barBorderRadius: 5,
+				// buttonBackgroundColor: '#EC7063',
+				// buttonBorderRadius: 5
+			},
+			tooltip: {
+			    	pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+			    	valueDecimals: 0,
+			    	useHTML: true
+			},
+			series: [{
+				name: 'مجموع تقارير',
+				data: posts,
+			}, {
+			 	name: 'تقارير موثوق',
+			 	data: trust,
+			}, {
+				name: 'تقارير غير موثوق بها',
+				data: untrust,
+			}]
+		});
+	}
+})();
--- js/regions.js
+++ js/regions.js
@@ -1,104 +1,143 @@
 (function() {
-	'use strict'
+  'use strict'
 
-	var csv = dsv(',');
-	var colors = '#1f77b4 #aec7e8 #2ca02c #98df8a #d62728 #DD514C #9467bd #c5b0d5 #e377c2 #dbdb8d #17becf #9edae5 #f90'.split(' ');
-	var atolls = [];
-	var islands = [];
-	
-	$.ajax({
-	    url: 'data/data-regions.csv',
-	    success: parseCSV
-	});
-	
-	
-	function parseCSV(raw) {
-	    var data = csv.parse(raw);
-	    // console.log(data)
-	
-	    var c = 0;
-	    var tmpAtolls = {};
-	    data.forEach(function(d) {
-	        if (!(d.atoll && d.island)) { return }
-	        var atoll = tmpAtolls[d.atoll] || { y: 0, islands: {}, name: d.atoll, color: colors[c++] };
-	        atoll.y++;
-	        var island = atoll.islands[d.island] = atoll.islands[d.island] || { y: 0, name: d.island, color: atoll.color };
-	        island.y++;
-	        tmpAtolls[d.atoll] = atoll;
-	    });
-	
-	    // console.log(tmpAtolls);
-	
-	    for (var tmp in tmpAtolls) {
-	        atolls.push(tmpAtolls[tmp]);
-	    }
-	    atolls.sort(function(a,b) { return b.y - a.y });
-	
-	    atolls.forEach(function(d) {
-	        var tmp = [];
-	        for (var islandName in d.islands) {
-	        	var island = d.islands[islandName]
-	            island.color = Highcharts.Color(island.color).brighten(0.1).get('rgb')
-	            tmp.push(island);
-	        }
-	        tmp.sort(function(a,b) { return b.y - a.y });
-	        islands = islands.concat(tmp);
-	    });
-	
-	    // console.log(atolls);
-	    // console.log(islands);
-	    render();
-	}
-	
-	function render() {
-	    $('#regions').highcharts({
-	        chart: {
-	            type: 'pie'
-	        },
-	        credits: {
-	        	enabled: false
-	        },
-	        title: {
-	            text: 'Incidents by Region (Atoll + Island)'
-	        },
-	        yAxis: {
-	            title: {
-	                text: 'Number of incidents'
-	            }
-	        },
-	        plotOptions: {
-	            pie: {
-	                shadow: false,
-	                center: ['50%', '50%']
-	            }
-	        },
-	        tooltip: {
-	         valueSuffix: ' incident(s)'
-	        },
-	        series: [{
-	            name: 'Atolls',
-	            data: atolls,
-	            size: '60%',
-	            dataLabels: {
-	                enabled: false,
-	                formatter: function() {
-	                    return this.point.name;
-	                },
-	                color: 'white',
-	                distance: -30
-	            }
-	        }, {
-	            name: 'Islands',
-	            data: islands,
-	            size: '80%',
-	            innerSize: '60%',
-	            dataLabels: {
-	                formatter: function() {
-	                    // display only if larger than 1
-	                    return this.y > 1 ? '<b>' + this.point.name + ':</b> ' + this.y + ' incidents' : null;
-	                }
-	            }
-	        }]
-	    });
-	}
+  var csv = dsv(',');
+  var colors = '#1f77b4 #aec7e8 #2ca02c #98df8a #d62728 #DD514C #9467bd #c5b0d5 #e377c2 #dbdb8d #17becf #9edae5 #f90'.split(' ');
+  var atolls = [];
+  var islands = [];
+
+  $.ajax({
+      url: 'data/data-regions.csv',
+      success: parseCSV
+  });
+
+
+  function parseCSV(raw) {
+      var data = csv.parse(raw);
+      // console.log(data)
+
+      var c = 0;
+      var tmpAtolls = {};
+      data.forEach(function(d) {
+          if (!(d.atoll && d.island)) { return }
+          var atoll = tmpAtolls[d.atoll] || { y: 0, islands: {}, name: d.atoll, color: colors[c++] };
+          atoll.y++;
+      // add opacity effect to outer (sub)regions to distinguish from atolls
+          var island = atoll.islands[d.island] = atoll.islands[d.island] || { y: 0, name: d.island, color: atoll.color };
+          island.y++;
+          tmpAtolls[d.atoll] = atoll;
+      });
+
+      // console.log(tmpAtolls);
+
+      for (var tmp in tmpAtolls) {
+          atolls.push(tmpAtolls[tmp]);
+      }
+      atolls.sort(function(a,b) { return b.y - a.y });
+
+      atolls.forEach(function(d) {
+          var tmp = [];
+          for (var islandName in d.islands) {
+            var island = d.islands[islandName]
+              island.color = Highcharts.Color(island.color).brighten(0.1).get('rgb')
+              tmp.push(island);
+          }
+          tmp.sort(function(a,b) { return b.y - a.y });
+          islands = islands.concat(tmp);
+      });
+
+      // console.log(atolls);
+      // console.log(islands);
+      render();
+  }
+
+  function render() {
+      $('#regions').highcharts({
+          chart: {
+              type: 'pie'
+          },
+          credits: {
+            enabled: false
+          },
+          title: {
+              text: 'Incidents by Region (Atoll + Island)'
+          },
+          yAxis: {
+              title: {
+                  text: 'Number of incidents'
+              }
+          },
+          // legend: {
+          //     enabled: true,
+          //     itemStyle: {
+          //         color: '#CCC'
+          //     },
+          //     itemHoverStyle: {
+          //         color: '#FFF'
+          //     },
+          //     itemHiddenStyle: {
+          //         color: '#333'
+          //     }
+          // },
+          plotOptions: {
+              pie: {
+                  shadow: false,
+                  center: ['50%', '50%']
+              }, 
+              series: {
+                point: {
+                  events: {
+                    legendItemClick: function() {
+                      var id = this.id,
+                      data = this.series.chart.series[0].data;
+                      $.each(data, function (i, point) {
+                        if (point.atolls == id) {
+                          if (point.visible)
+                              point.setVisisble(false);
+                          else
+                              point.setVisible(true);
+                        }
+                      });
+                    }
+                  }
+                }
+              }
+          },
+          legend: {
+              layout: 'vertical',
+              floating: true,
+              align: 'right',
+              verticalAlign: 'top',
+              symbolPadding: 20, 
+              symbolWidth: 10
+          },
+          tooltip: {
+           valueSuffix: ' incident(s)'
+          },
+          series: [{
+              name: 'Atolls',
+              data: atolls,
+              size: '60%',
+              dataLabels: {
+                  enabled: false,
+                  formatter: function() {
+                      return this.point.name;
+                  },
+                  color: 'white',
+                  distance: -30
+              }
+          }, {
+              name: 'Islands',
+              data: islands,
+              size: '80%',
+              innerSize: '60%',
+              dataLabels: {
+                  formatter: function() {
+                      // display only if larger than 1
+                      return this.y > 1 ? '<b>' + this.point.name + ':</b> ' + this.y + ' incidents' : null;
+                  }
+              }
+          }]
+      });
+  }
 })();
