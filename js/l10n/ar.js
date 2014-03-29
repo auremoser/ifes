dcacd16205c09d09807547ffd24cc4c547dbb65b
Aurelia Moser <auremoser@gmail.com>
2014-03-30 00:15:15 +0300

    experimental solve for translation issue

---
+++ js/l10n/ar.js
@@ -0,0 +1,12 @@
+if(!window['l10n']) window.l10n = {};
+l10n.ar = {
+   'Total Reports': 'مجموع تقارير',
+   'Trusted': 'تقارير موثوق',
+   'Untrusted': 'تقارير غير موثوق بها',
+   'Number of Reports':'عدد من تقارير',
+   'Ushahidi Report Counts': 'تقرير تعقب INTL Incident Count',
+   '1M':'1 شهر',
+   '3M':'3 شهر',
+   '1Y': '1 سنة',
+   'All':'كل',
+};
---
+++ js/l10n/en.js
@@ -0,0 +1,12 @@
+if(!window['l10n']) window.l10n = {};
+l10n.en = {
+   'Total Reports': 'Total Reports',
+   'Trusted': 'Trusted',
+   'Untrusted': 'Untrusted',
+   'Number of Reports':'Number of Reports',
+   'Ushahidi Report Counts': 'Ushahidi Report Counts',
+   '1M':'1M',
+   '3M':'3M',
+   '1Y': '1Y',
+   'All':'All',
+};
